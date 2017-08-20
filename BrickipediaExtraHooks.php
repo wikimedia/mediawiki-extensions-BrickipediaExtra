51a5b8ad189b9835de278a864951b4d0a708177e
SamanthaNguyen <samanthanguyen1116@gmail.com>
2017-08-20 09:48:05 -0500

    Initial commit import from ShoutWiki trunk
    This extension is specific to Brickipedia, an online encyclopedia
    about LEGO. This provides custom messages for the Brickipedia Wiki.
    
    Change-Id: Ic15f26a0171594ea416ca59f65c22bd6386c2a3a

---
+++ BrickipediaExtraHooks.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This mini-extension adds two links to the page footer.
+ * In addition to that, all Brickipedia-specific i18n messages are contained
+ * in this extension.
+ *
+ * @file
+ * @date 9 February 2017
+ */
+class BrickipediaExtraHooks {
+
+	public static function onSkinTemplateOutputPageBeforeExec( $sk, &$tpl ) {
+		// "Terms of Use" link in footer
+		/*
+		$tpl->set( 'termsofuse', $sk->footerLink( 'termsofuse', 'termsofusepage' ) );
+		$tpl->data['footerlinks']['places'][] = 'termsofuse';
+		*/
+
+		// "Parents" link in footer
+		$tpl->set( 'parents', $sk->footerLink( 'parents', 'parentspage' ) );
+		$tpl->data['footerlinks']['places'][] = 'parents';
+
+		return true;
+	}
+
+}
