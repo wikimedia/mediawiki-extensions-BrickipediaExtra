7b58d82304465b1a253e54c8ef437632236e3d6a
Jack Phoenix <ashley@uncyclomedia.co>
2026-03-01 15:05:03 +0200

    Fix extension for MediaWiki 1.43+ where Skin#footerLink has been removed from core
    Code sporked from the Nimbus skin, where I sporked it from MW 1.39 core; I claim no ownership of this method.
    
    Thanks to Lcawte for pointing out this!
    
    Change-Id: I0bc76af8336b029183358ad61273fbb329e5192c

--- BrickipediaExtraHooks.php
+++ BrickipediaExtraHooks.php
@@ -9,8 +9,12 @@
  * in this extension.
  *
  * @file
- * @date 2 November 2020
+ * @date 1 March 2026
  */
+
+use MediaWiki\Html\Html;
+use MediaWiki\Title\Title;
+
 class BrickipediaExtraHooks {
 
 	/**
@@ -23,14 +27,65 @@ class BrickipediaExtraHooks {
 		// "Terms of Use" link in footer
 		/*
 		if ( $key === 'places' ) {
-			$footerLinks['termsofuse'] = $skin->footerLink( 'termsofuse', 'termsofusepage' );
+			$footerLinks['termsofuse'] = self::footerLink( $skin, 'termsofuse', 'termsofusepage' );
 		}
 		*/
 
 		// "Parents" link in footer
 		// Just a lil' healthy dose of some old-fashioned paranoia...
 		if ( $key === 'places' ) {
-			$footerLinks['parents'] = $skin->footerLink( 'parents', 'parentspage' );
+			$footerLinks['parents'] = self::footerLink( $skin, 'parents', 'parentspage' );
 		}
 	}
+
+	/**
+	 * Sporked from MW core includes/skin/Skin.php as of MW 1.39.
+	 * The only real differences compared to the footerLink method there
+	 * are that:
+	 * - this also integrates the guts of the footerLinkTitle() method.
+	 * - this method takes $skin as the 1st param instead of using $this
+	 *
+	 * @see https://phabricator.wikimedia.org/T326109
+	 *
+	 * Given a pair of message keys for link and text label,
+	 * return an HTML link for use in the footer.
+	 *
+	 * @param Skin $skin
+	 * @param string $desc The i18n message key for the link text.
+	 * 		The content of this message will be the visible text label.
+	 * 		If this is set to nonexisting message key or the message is
+	 * 		disabled, the link will not be generated, empty string will
+	 * 		be returned in the stead.
+	 * @param string $page The i18n message key for the page to link to.
+	 * 		The content of this message will be the destination page for
+	 * 		the footer link. Given a message key 'Privacypage' with content
+	 * 		'Project:Privacy policy', the link will lead to the wiki page with
+	 * 		the title of the content.
+	 *
+	 * @return string HTML anchor
+	 */
+	public static function footerLink( $skin, $desc, $page ) {
+		// If the link description has been disabled in the default language,
+		if ( $skin->msg( $desc )->inContentLanguage()->isDisabled() ) {
+			// then it is disabled, for all languages.
+			return '';
+		}
+
+		// Otherwise, we display the link for the user, described in their
+		// language (which may or may not be the same as the default language),
+		// but we make the link target be the one site-wide page.
+		$title = Title::newFromText( $skin->msg( $page )->inContentLanguage()->text() );
+		if ( !$title ) {
+			return '';
+		}
+
+		// Similar to Skin::addToSidebarPlain
+		// Optimization: Avoid LinkRenderer here as it requires extra DB info
+		// to add unneeded classes even for makeKnownLink (T313462).
+		return Html::element( 'a',
+			[ 'href' => $title->fixSpecialName()->getLinkURL() ],
+			$skin->msg( $desc )->text()
+		);
+	}
+
 }
